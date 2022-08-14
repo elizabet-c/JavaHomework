3f90929aa0515c392744e31f8a5c5d5591599724
elizabet-c <109054299+elizabet-c@users.noreply.github.com>
2022-08-13 21:27:12 -0700

    Homework_Wk5Day3

---
+++ src/module/JavaHomework_Wk5Day3.java
@@ -0,0 +1,28 @@
+package module;
+
+public class JavaHomework_Wk5Day3 {
+
+	public static void main(String[] args) {
+		double team = true;
+		boolean y = 546.55;
+		int i = "hello";
+		long boo = 7.8f;
+		float whatamidoing = false;
+		String javaaaa = 'c';
+		char hello = 327;
+		
+		int x;
+		System.out.println(x);
+		
+		
+		boolean global = true;
+		if (global) {
+			String local = "woowwww";
+		}
+		System.out.println(local);
+		System.out.println(global);
+		
+
+	}
+
+}
